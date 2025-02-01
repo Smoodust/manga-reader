094ddc35c76a11349dc7d37fe6c60ab1b01b53d3
Smoodust <42073101+Smoodust@users.noreply.github.com>
2025-02-02 02:06:38 +0400

    Quick fix

--- frontend/src/tsx/Main.tsx
+++ frontend/src/tsx/Main.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import ImageOpenDialog from "./ImageOpenDialog";
 import NoMangaScreen from "./NoMangaScreen";
 import MangaDraw from "./MangaDraw";
